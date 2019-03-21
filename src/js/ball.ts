b78f517b37fe0d5ff40ceda170b186cbe1b63c31
sofu0185 <sofu0185@edu.easj.dk>
2019-03-21 19:36:14 +0100

    changed ball to be a circle

--- src/js/ball.ts
+++ src/js/ball.ts
@@ -12,8 +12,9 @@ export class Ball implements GameObject
     public position:Vector;
     private direction:Vector;
     private speed:number = 80;
-    private size:number= 10;
+    private size:number= 20;
     private color: string = "white";
+    private centerPoint: Vector;
 
     constructor (position:Vector, gameEngine:GameEngine, direction:Vector)
     {
@@ -22,6 +23,9 @@ export class Ball implements GameObject
         this.gameEngine = gameEngine;
         this.height = this.size;
         this.width = this.size;
+        // calculate centerpoint
+        this.centerPoint.x = this.position.x + this.width / 2;
+        this.centerPoint.y = this.position.y + this.height / 2;
     }
 
     // Update method takes care of all logic
@@ -56,7 +60,11 @@ export class Ball implements GameObject
     // draw ball on canvas
     draw(ctx: CanvasRenderingContext2D): void {
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.position.x, this.position.y, this.size, this.size);
+        ctx.beginPath();
+        // cirel is drawn from center, so x and y needs to be offset by the balls radius
+        ctx.arc(this.position.x + this.size / 2, this.position.y + this.size / 2, this.size / 2, 0, 2 * Math.PI);
+        ctx.fill();
+        //ctx.fillRect(this.position.x, this.position.y, this.size, this.size);
     }
     
     // in case of any collision this method is called
@@ -68,8 +76,10 @@ export class Ball implements GameObject
 
         // reverse direction if ball collides with any object other than framerate
         if(!(other instanceof Framerate))
-        {
+        {          
+            
             this.direction.x *= -1;
+            
         }
             
     }
